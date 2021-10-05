c350ec3aa94e744e81c26483626d6b9c1c66f36c
­ <bemyak@pm.me>
2021-10-05 23:54:41 +0300

    Solved up to chapter 8

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -2,14 +2,47 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::{entry, Delay, DelayMs, LedArray, OutputSwitch};
+
+enum Action {
+    On,
+    Off,
+}
+
+impl Action {
+    fn toggle(&mut self) {
+        match self {
+            Action::On => *self = Action::Off,
+            Action::Off => *self = Action::On,
+        }
+    }
+}
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let half_period = 50_u8;
+
+    let mut i = 0_u8;
+    let mut action = Action::On;
+
+    leds[0].on().ok();
+
+    loop {
+        match action {
+            Action::On => {
+                let i = i.wrapping_add(1);
+                let j = i as usize % leds.len();
+                leds[j].on().ok();
+            }
+            Action::Off => {
+                let j = i as usize % leds.len();
+                leds[j].off().ok();
+                i = i.wrapping_add(1);
+            }
+        }
+        action.toggle();
+        delay.delay_ms(half_period);
+    }
 }
--- src/07-registers/src/main.rs
+++ src/07-registers/src/main.rs
@@ -2,28 +2,22 @@
 #![no_std]
 
 #[allow(unused_imports)]
-use aux7::{entry, iprint, iprintln};
+use aux7::{entry, iprintln, RegisterBlock, ITM};
 
 #[entry]
 fn main() -> ! {
-    aux7::init();
+    let gpioe = aux7::init().1;
 
-    unsafe {
-        // A magic address!
-        const GPIOE_BSRR: u32 = 0x48001018;
-
-        // Turn on the "North" LED (red)
-        *(GPIOE_BSRR as *mut u32) = 1 << 9;
-
-        // Turn on the "East" LED (green)
-        *(GPIOE_BSRR as *mut u32) = 1 << 11;
-
-        // Turn off the "North" LED
-        *(GPIOE_BSRR as *mut u32) = 1 << (9 + 16);
-
-        // Turn off the "East" LED
-        *(GPIOE_BSRR as *mut u32) = 1 << (11 + 16);
-    }
+    gpioe.odr.write(|w| {
+        w.odr8().set_bit();
+        w.odr9().set_bit();
+        w.odr10().set_bit();
+        w.odr11().set_bit();
+        w.odr12().set_bit();
+        w.odr13().set_bit();
+        w.odr14().set_bit();
+        w.odr15().set_bit()
+    });
 
     loop {}
 }
