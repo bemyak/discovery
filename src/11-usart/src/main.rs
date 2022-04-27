46908abdb7d464f07d0853b0a0d3e28c80ef8ff0
­ <bemyak@pm.me>
2022-04-27 21:11:03 +0300

    Add solutions

--- src/11-usart/src/main.rs
+++ src/11-usart/src/main.rs
@@ -1,17 +1,29 @@
+#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
 #[allow(unused_imports)]
 use aux11::{entry, iprint, iprintln};
+use heapless::Vec;
 
 #[entry]
 fn main() -> ! {
     let (usart1, _mono_timer, _itm) = aux11::init();
 
-    // Send a single character
-    usart1
-        .tdr
-        .write(|w| w.tdr().bits(u16::from(b'X')) );
+    // A buffer with 32 bytes of capacity
+    let mut buffer: Vec<u8, 32> = Vec::new();
 
-    loop {}
+    loop {
+        buffer.clear();
+
+        loop {
+            while usart1.isr.read().rxne().bit_is_clear() {}
+            // Retrieve the data
+            let b = usart1.rdr.read().rdr().bits() as u8;
+            while usart1.isr.read().txe().bit_is_clear() {}
+            usart1.tdr.write(|w| w.tdr().bits(b as u16));
+           
+        }
+    }
 }
+// AT+BAUD8
